8f98ebefe112639930d5c9c8e4ca0fc658a25bf3
Ton Schoots <ton@maiastra.com>
2016-08-03 14:42:22 +0200

    fixed build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
 rm -rf atomic_scanner
 
-GO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o atomic_scanner
+CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o atomic_scanner
